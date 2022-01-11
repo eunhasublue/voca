e3c15b2ee5e7d0b327f644726de75748e9a17c5a
eunhasublue <eunhasublooming@gmail.com>
2022-01-11 22:28:07 +0900

    #11 JSON-Server & REST API

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -1,10 +1,14 @@
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import dummy from "../db/data.json";
 
 export default function DayList() {
+  const [days, setDays] = useState([]);
+  const [count, setCount] = useState(0);
+  useEffect(() => {});
   return (
     <ul className="list_day">
-      {dummy.days.map((day) => (
+      {days.map((day) => (
         <li key={day.id}>
           <Link to={`/day/${day.day}`}> Day {day.day}</Link>
         </li>
