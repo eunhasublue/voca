950ea1b6bea776b521fd832928a068a92b502b67
eunhasublue <eunhasublooming@gmail.com>
2022-01-12 14:07:04 +0900

    #14 REST API : PUT, DELETE

--- src/components/Word.js
+++ src/components/Word.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
 
-export default function Word({ word }) {
+export default function Word({ word: w }) {
+  const [word, setWords] = useState(w);
   const [isShow, setIsShow] = useState(false);
   const [isDone, setIsDone] = useState(word.isDone);
 
@@ -14,15 +15,40 @@ export default function Word({ word }) {
     // 1. 2번째 인자로 객체를 넣어줌
     //  1-1. 객체에는 요청의 옵션을 입력
     fetch(`http://localhost:3001/words/${word.id}`, {
+      // put : 수정(update)
       method: "PUT",
       headers: {
         // Content-Type : 보내는 리소스의 타입
         "Content-Type": "application/json",
       },
-      body,
+      // put은 수정에 필요한 정보를 보내줘야 함 -> body에 입력
+      // JSON.stringify : json 문자열로 변환
+      body: JSON.stringify({
+        ...word,
+        isDone: !isDone,
+      }),
+    }).then((res) => {
+      if (res.ok) {
+        setIsDone(!isDone);
+      }
     });
   }
 
+  function del() {
+    if (window.confirm("삭제 하시겠습니까?")) {
+      fetch(`http://localhost:3001/words/${word.id}`, {
+        method: "DELETE",
+      }).then((res) => {
+        if (res.ok) {
+          setWords({ id: 0 });
+        }
+      });
+    }
+  }
+
+  if (word.id === 0) {
+    return null;
+  }
   return (
     <tr className={isDone ? "off" : ""}>
       <td>
@@ -32,7 +58,9 @@ export default function Word({ word }) {
       <td>{isShow && word.kor}</td>
       <td>
         <button onClick={toggleShow}>뜻 {isShow ? "숨기기" : "보기"}</button>
-        <button className="btn_del">삭제</button>
+        <button onClick={del} className="btn_del">
+          삭제
+        </button>
       </td>
     </tr>
   );
